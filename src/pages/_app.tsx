e7d35dd3d1a6225937705c45678dcea54b9c0c55
theyarestories <theyarestories@gmail.com>
2023-10-31 08:15:28 +0200

    Setup next intl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { NextIntlProvider } from "next-intl";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Translations
+  const router = useRouter();
+  const translations = require(`@/translations/${router.locale}.json`);
+
+  return (
+    <NextIntlProvider messages={translations}>
+      <Component {...pageProps} />
+    </NextIntlProvider>
+  );
 }
