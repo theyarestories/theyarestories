adfff4247451073b6a2d1ac437d714e7f9a32900
theyarestories <theyarestories@gmail.com>
2023-12-22 22:17:02 +0200

    Added back env variable ts

--- src/interfaces/process.d.ts
+++ src/interfaces/process.d.ts
@@ -4,5 +4,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_DETECT_LANGUAGE_API_KEY: string;
     CLOUDINARY_API_SECRET: string;
     NEXT_PUBLIC_CLOUDINARY_API_KEY: string;
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
   }
 }
