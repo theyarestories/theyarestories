cae34e83b2305de6b56a55d8b21b267924342505
theyarestories <theyarestories@gmail.com>
2023-12-27 01:43:53 +0200

    Added header to homepage

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,16 @@
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 type Props = {};
 
 function Logo({}: Props) {
   const t = useTranslations("Logo");
 
-  return <h1 className="font-bold">{t("logo")}</h1>;
+  return (
+    <Link href={"/"}>
+      <h1 className="font-bold">{t("logo")}</h1>
+    </Link>
+  );
 }
 
 export default Logo;
