7876a590058de78d074bf2af4f76a024c07ea353
theyarestories <theyarestories@gmail.com>
2024-01-07 22:16:05 +0200

    Added approvedBy to stories schema

--- src/interfaces/database/DBStory.ts
+++ src/interfaces/database/DBStory.ts
@@ -14,6 +14,7 @@ export interface DBTranslatedFields {
   story: string;
   job?: string;
   isApproved: boolean;
+  approvedBy: string | null;
   createdAt: string;
   updatedAt: string;
 }
@@ -35,6 +36,7 @@ export type DBStory = {
   translationLanguage: string; // ar
   translations: DBTranslatedFields[];
   isApproved: boolean;
+  approvedBy: string | null;
   isHighlighted: boolean;
   isDeleted: boolean;
   createdAt: string;
