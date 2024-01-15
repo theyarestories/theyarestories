a93c01343c5a0b316fb54bb2d32016ac2a5709bb
Moaaz Bhnas <111786023+moaazbhnas228@users.noreply.github.com>
2024-01-15 03:27:54 +0200

    Added footer to all stories page

--- src/pages/all-stories.tsx
+++ src/pages/all-stories.tsx
@@ -102,7 +102,11 @@ export default function AllStoriesPage({
   }, [router.query.page, router.locale, router.query.search]);
 
   return (
-    <Layout pageTitle={t("page_title")} pageDescription={t("page_description")}>
+    <Layout
+      pageTitle={t("page_title")}
+      pageDescription={t("page_description")}
+      withFooter
+    >
       <Container>
         <div className="space-y-4">
           {/* Search form */}
