a11b01fcb349269c1e38abe847cc9648f9b264da
theyarestories <theyarestories@gmail.com>
2024-01-26 14:53:40 +0200

    Small change

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -6,7 +6,6 @@ function Banner({}: Props) {
   const t = useTranslations("Banner");
 
   return (
-    // border
     <p className="gap-1 font-display text-gray-600 sm:text-2xl text-xl flex flex-col p-4 sm:p-6">
       <span>{t("not_number")}</span>
       <span>{t("but_stories")}</span>
